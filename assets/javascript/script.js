ec5ef89f9cf180ed42fa426e3eb13a1dbf1710af
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-04-20 17:12:30 -0600

    added new files for logo

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCTlCEPrbZ3EBwGJeUyVfkI3p54fJP8Bec",
+    authDomain: "jccall-portfolio.firebaseapp.com",
+    databaseURL: "https://jccall-portfolio.firebaseio.com",
+    projectId: "jccall-portfolio",
+    storageBucket: "jccall-portfolio.appspot.com",
+    messagingSenderId: "879674023834",
+    appId: "1:879674023834:web:fbe3e76f046e47c99ae423"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
